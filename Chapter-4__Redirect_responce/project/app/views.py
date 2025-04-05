9b67c972171154fa03594a449b83025acb1835eb
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-05 13:38:53 +0530

    new code

--- Chapter-4__Redirect_responce/project/app/views.py
+++ Chapter-4__Redirect_responce/project/app/views.py
@@ -3,17 +3,14 @@ from django.shortcuts import render,redirect # type: ignore
 # Create your views here.
 
 def home(request):
-    # return redirect('https://www.google.com/')
-    # return redirect('/index/')
- 
     name , city = 'vineet' , 'Bhopal'
     return redirect(f'/index/?name={name}&city={city}')
 
 
 
 def index(request):
-    print(request.method)
-    print(request.GET)
+    print("REEEEEEEEEEEEEEEEEEEEE",request.method) 
+    print("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRR",request.GET)
     x = request.GET.get('name')
     y = request.GET.get('city')
     print(x , y)
