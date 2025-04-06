46743d63452abba0003c4211e35f1170f8152997
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-06 23:24:32 +0530

    new code

--- Chapter-5__Template_Cutting/project/app/views.py
+++ Chapter-5__Template_Cutting/project/app/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+
+
 def home(request):
-    return HttpResponse('<h1>This Is Home Page :</h1>')
+    return HttpResponse('<h1>This Is Home Page  for randering:</h1>')
