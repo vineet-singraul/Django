55db414c94225c02b531a2f575cf1f4537e6a801
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-08 22:34:12 +0530

    new code

--- Chapter-5__Template_Cutting/project/app/views.py
+++ Chapter-5__Template_Cutting/project/app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
-
-
-
 def home(request):
-    return HttpResponse('<h1>This Is Home Page  for randering:</h1>')
+    return render(request , 'home.html')
+
+def about(request):
+    return render(request , 'about.html')
+
+def contact(request):
+    return render(request , 'contact.html')
+
+def register(request):
+    return render(request , 'register.html')
+
+def login(request):
+    return render(request , 'login.html')
--- Chapter-5__Template_Cutting/project/project/urls.py
+++ Chapter-5__Template_Cutting/project/project/urls.py
@@ -19,5 +19,9 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home , name='home')
+    path('',views.home , name='home'),
+    path('about/',views.about , name='about'),
+    path('contact/',views.contact , name='contact'),
+    path('register/',views.register , name='register'),
+    path('login/',views.login , name='login')
 ]
