8052ae2e9a84d509192624106c10a5c4f1021fa5
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-10 23:53:16 +0530

    new code

--- Chapter-5__Template_Cutting/project/app/views.py
+++ Chapter-5__Template_Cutting/project/app/views.py
@@ -16,10 +16,22 @@ def register(request):
 def registerdata(request):
     print(request.method)
     print(request.POST)
-    print(request.GET)
-    print(request.FILES)
-    print(request.COOKIES)
-    print(request.META)
+    # print(request.GET)
+    # print(request.FILES)
+    # print(request.COOKIES)
+    # print(request.META)
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    detail = request.POST.get('detail')
+    phone = request.POST.get('phone')
+    dob = request.POST.get('dob')
+    subscribe = request.POST.getlist('subscribe')
+    gender = request.POST.get('gender')
+    password = request.POST.get('password')
+    cpassword = request.POST.get('cpassword')
+    
+    print(username,email,detail,phone,dob,subscribe,gender,password,cpassword)
+
 
 def login(request):
     return render(request , 'login.html')
