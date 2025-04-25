ab4cc8f210fbdf971dbe21436f1a5a9ff014c27e
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-25 18:20:23 +0530

    new code:

---
+++ Django_1_Cybrom/Chapter-12-Proxy-User/project/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Mainmodel
+# Register your models here.
+
+
+admin.site.register(Mainmodel)
---
+++ Django_1_Cybrom/Chapter-12-Proxy-User/project/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Mainmodel(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    contect = models.CharField(max_length=40)
+    add = models.CharField(max_length=40)
+
+
+class ProxyModel(Mainmodel):
+    class Meta:
+        proxy = True
