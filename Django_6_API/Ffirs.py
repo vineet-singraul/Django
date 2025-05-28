861e05e904ae715872f5b054048eca2e7198c7c4
Vineet Singraul <singraulvineet943@gmail.com>
2025-05-28 19:24:27 +0530

    new code

---
+++ Django_6_API/Ffirs.py
@@ -0,0 +1,19 @@
+import json
+
+p_data = {
+    'name':True,
+    'age':False,
+    'quali':None,
+    'active':'yas'
+}
+
+json_data = json.dumps(p_data)
+print(json_data)
+print(type(json_data))
+
+json_data1 = '{"name": true, "age": false, "quali": null, "active": "yas"}'
+
+python_data = json.loads(json_data1)
+print(python_data)
+print(type(python_data))
+
