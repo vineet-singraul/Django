a2677ead01ebe3c179e149ab247ca468ac338e01
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-09 23:59:17 +0530

    new code

--- Django_6_API/L1-API/project/app/admin.py
+++ Django_6_API/L1-API/project/app/admin.py
@@ -3,5 +3,5 @@ from app.models import StreamPlatform,WatchList
 # Register your models here.
 
 
-admin.site.register(StreamPlatform)
+admin.site.register(StreamPlatform)           
 admin.site.register(WatchList)   
